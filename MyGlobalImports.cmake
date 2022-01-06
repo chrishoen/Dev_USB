15b3c6a1785148845a8f0073272337725ebfacd9
chrishoen <chris123hoen@gmail.com>
2022-01-06 11:26:28 -0500

    new rislib with readline

--- MyGlobalImports.cmake
+++ MyGlobalImports.cmake
@@ -44,6 +44,7 @@ function(my_lib_import_RisLib _target)
       target_link_libraries(${_target} RisLib)
       target_link_libraries(${_target} pthread)
       target_link_libraries(${_target} rt)
+      target_link_libraries(${_target} readline)
    endif()
 
 endfunction()
